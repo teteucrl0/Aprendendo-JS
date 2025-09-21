0f88f477b056837e13662cd4c82f1e2118b5443d
teteucrl0 <teteupoxinha@gmail.com>
2025-09-21 12:02:40 -0300

    Algumas funções com arrays

---
+++ arrays.js
@@ -0,0 +1,8 @@
+const nomes = ['Matheus','Joao','Tamiris'];
+
+nomes.push('Felipe') // ADICIONA UM NOVO ELEMENTO NA ULTIMA POSICAO DO ARRAY
+nomes.unshift('Maria') //ADICIONA UM NOVO ELEMENTO NA PRIMEIRA POSICAO DO ARRAY
+nomes.shift(); //RETIRA O PRIMEIRO ELEMENTO DO ARRAY
+nomes.pop(); //RETIRA O ULTIMO ELEMENTO DO ARRAY
+
+console.log(nomes)
