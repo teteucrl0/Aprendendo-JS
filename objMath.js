b72b4bd38d0f300b2e4517fa77a5e78c26442a72
teteucrl0 <teteupoxinha@gmail.com>
2025-09-21 10:43:10 -0300

    Aprendendo sobre o objeto Math

---
+++ objMath.js
@@ -0,0 +1,11 @@
+let num1 = 9.32323942
+
+let floor = Math.floor(num1) 
+let ceil = Math.ceil(num1)
+let round = Math.round(num1)
+let aleatorio = Math.floor(Math.random() * 15)
+
+console.log(floor)
+console.log(ceil)
+console.log(round)
+console.log(aleatorio)
