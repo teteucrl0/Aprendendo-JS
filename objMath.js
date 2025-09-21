37107cbd37e1c5ab511ff35dc39e38ec77394fb1
teteucrl0 <teteupoxinha@gmail.com>
2025-09-21 10:54:04 -0300

    Aprendendo sobre o Objeto Math (O BASICO)

--- objMath.js
+++ objMath.js
@@ -4,7 +4,7 @@ const floor = Math.floor(num1); // ARREDONDA PARA BAIXO
 const ceil = Math.ceil(num1); //ARRENDONDA PARA CIMA
 const round = Math.round(num1); // ARREDONDA PARA O PRÓXIMO
 const aleatorio = Math.floor(Math.random() * 15); // CRIA UM NUMERO INTEIRO ALEATORIO DE 0 A 14
-const PI = Math.PI(); // MOSTRA O VALOR DE PI
+const PI = Math.PI; // MOSTRA O VALOR DE PI
 const elevado = Math.pow(5, 20); //FUNÇÃO PARA ELEVAR OS NUMEROS
 
 console.log(floor);
