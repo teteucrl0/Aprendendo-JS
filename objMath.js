578eab943bd0ddffdebcca6d8e01b1c0ab5dd39f
teteucrl0 <teteupoxinha@gmail.com>
2025-09-21 10:48:34 -0300

    Aprendendo sobre o Objeto Math (O BASICO)

--- objMath.js
+++ objMath.js
@@ -2,7 +2,7 @@ let num1 = 9.32323942
 
 let floor = Math.floor(num1) // ARREDONDA PARA BAIXO
 let ceil = Math.ceil(num1) //ARRENDONDA PARA CIMA
-let round = Math.round(num1) // ARREDONDA O NUMERO FLUTUANTE
+let round = Math.round(num1) // ARREDONDA PARA O PRÓXIMO
 let aleatorio = Math.floor(Math.random() * 15) // CRIA UM NUMERO INTEIRO ALEATORIO DE 0 A 14
 
 console.log(floor)
